7f0f144207c2f68aeedbf331fad052fe6db107b0
Lance Ball <lball@redhat.com>
2023-09-25 17:44:15 -0400

    :open_file_folder: update Red Hat specific files

--- redhat/release/update-to-head.sh
+++ redhat/release/update-to-head.sh
@@ -67,12 +67,20 @@ if [[ -d redhat/patches ]]; then
   git apply redhat/patches/*
 fi
 
+# RHTAP writes its pipeline files to the root of ${redhat_ref}
+# Fetch those from origin and apply them to the the release branch
+# since we just wiped out our local copy with the upstream ref.
+git fetch origin $redhat_ref
+git checkout origin/$redhat_ref .tekton
+
+# Move overlays to root
+if [[ -d redhat/overlays ]]; then
+  git mv redhat/overlays/* .
+fi
+
 git add . # Adds applied patches
 git add $custom_files # Adds custom files
 git commit -m "${redhat_files_msg}"
-
-# Push the release-next branch
-git push -f origin "${redhat_ref}"
 
 # Trigger CI
 # TODO: Set up openshift or github CI to run on release-next-ci
